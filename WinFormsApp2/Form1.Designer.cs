98deb51a229ae9bdd4b74bb7f0a3c85de31bf357
Tiaren33i <mironovdanila04@gmail.com>
2023-04-26 19:37:39 +0300

    new

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.ForeColor = SystemColors.ControlText;
+            button1.Location = new Point(257, 95);
+            button1.Margin = new Padding(4);
+            button1.Name = "button1";
+            button1.Size = new Size(216, 40);
+            button1.TabIndex = 0;
+            button1.Text = "Играть";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(257, 158);
+            button2.Margin = new Padding(4);
+            button2.Name = "button2";
+            button2.Size = new Size(216, 40);
+            button2.TabIndex = 1;
+            button2.Text = "Об игре";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(257, 221);
+            button3.Margin = new Padding(4);
+            button3.Name = "button3";
+            button3.Size = new Size(216, 40);
+            button3.TabIndex = 2;
+            button3.Text = "Выход";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += Button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.AppWorkspace;
+            BackgroundImage = Properties.Resources._3072214;
+            ClientSize = new Size(734, 401);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "Рулетка";
+            TransparencyKey = Color.Black;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
